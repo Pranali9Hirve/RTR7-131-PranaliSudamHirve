26edabc86c050eb9d3eeed2c0a90f4f6c29e7be5
pranali-hirve <pranali.hirve@datenflow.com>
2026-09-01 00:46:02 +0530

    RTR7-131-PranaliSudamHirve/RTR-7/MyProjects/01-OpenGL/01-FFP/01-Windows/01-Windowing/06-CenteredWindow/02-SystemParameterInfo

---
+++ RTR-7/MyProjects/01-OpenGL/01-FFP/01-Windows/01-Windowing/06-CenteredWindow/02-SystemParameterInfo/Window.c
@@ -0,0 +1,97 @@
+// Header Files
+#include<windows.h> // Hi header file windows chi ahe
+
+// MACROS
+// 4X3
+#define PSH_WIN_WIDTH 800
+#define PSH_WIN_HEIGHT 600
+
+// Global Function Declarations
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+// Entry Point Function
+// HINSTANCE -> HANDLE
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+    // Variable Declarations
+    WNDCLASSEX wndclass;
+    HWND hwnd = NULL;
+    MSG msg;
+    TCHAR szAppName[] = TEXT("RTR7_PSH");
+
+    // Code
+    // 1: WNDCLASSEX Structure Initialization
+    wndclass.cbSize = sizeof(WNDCLASSEX); // Added newly cb-count of bytes (byte size)
+    wndclass.style = CS_HREDRAW|CS_VREDRAW; // CS -> class style 
+    wndclass.cbClsExtra = 0;
+    wndclass.cbWndExtra = 0;
+    wndclass.lpfnWndProc = WndProc;
+    wndclass.hInstance = hInstance;
+    wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndclass.lpszClassName = szAppName;
+    wndclass.lpszMenuName = NULL;
+    wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION); // Added newly
+
+    // 2: Register Above WNDCLASS
+    RegisterClassEx(&wndclass); // return value = atom (such a string which is immutable )
+
+    // Centering
+    RECT pshRcWorkArea;
+
+    SystemParametersInfo(
+        SPI_GETWORKAREA,
+        0,
+        &pshRcWorkArea,
+        0
+    );
+    // Farashi chi width
+    int pshScreenWidth = pshRcWorkArea.right - pshRcWorkArea.left;
+    // Farashi chi height
+    int pshScreenHeight = pshRcWorkArea.bottom - pshRcWorkArea.top;
+
+    // 3: Create the Window
+    hwnd = CreateWindow(
+        szAppName,
+        TEXT("Pranali Hirve: RTR7-131-PranaliSudamHirve/RTR-7/MyProjects/01-OpenGL/01-FFP/01-Windows/01-Windowing/06-CenteredWindow/02-SystemParameterInfo"),
+        WS_OVERLAPPEDWINDOW,
+        pshRcWorkArea.left + (pshScreenWidth - PSH_WIN_WIDTH) / 2, // x-coordinate, screen 
+        pshRcWorkArea.top + (pshScreenHeight - PSH_WIN_HEIGHT) / 2, // y-coordinate
+        PSH_WIN_WIDTH, // width, rumalachi width
+        PSH_WIN_HEIGHT, // height, rumalachi height
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
+
+    // Show Window
+    ShowWindow(hwnd, iCmdShow);
+
+    // Update the window to paint its backgound
+    UpdateWindow(hwnd);
+
+    // Message Loop
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return((int)msg.wParam);
+}
+
+// Declarator (Function Implementation)
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+    // Code
+    switch(iMsg)
+    {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        default:
+            break; 
+    }
+    return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
